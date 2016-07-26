6eae30794167c616a67922082934b9d89c2f6d0b
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-07-25 19:39:32 -0700

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -6,6 +6,25 @@ describe('Favorite Food Filter', function () {
 	beforeEach(inject(function ($injector) {
 		$filter = $injector.get('$filter');
 	}));
+ 
+    it('should filter the favorite food', function () {
+      var foodList = [{
+      name: 'Test',
+      favoriteFood: 'Potatoes'
+    }, {
+      name: 'Test 2',
+      favoriteFood: 'Bourbon'
+    }, {
+      name: 'Test 3',
+      favoriteFood: 'Oysters'
+    }];
+    
+    var results = $filter('favoriteFood')(foodList, 'Bourbon');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Test 2');
+
+    });
 
 	
 });
